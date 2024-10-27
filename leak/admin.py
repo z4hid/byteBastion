5c15614fe917ca36f0fbd34d795b9982fce1ef7c
z4hid <zahidhasan9297@gmail.com>
2024-10-27 21:40:14 +0600

    Added LeakResult model admin interface to leak/admin.py

--- leak/admin.py
+++ leak/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+# leak/admin.py
+from django.contrib import admin
+from .models import LeakResult
+@admin.register(LeakResult)
+class LeakCheckResultAdmin(admin.ModelAdmin):
+    list_display = ('username_or_email', 'found_count', 'checked_at')
+    search_fields = ('username_or_email',)
+    readonly_fields = ('checked_at',)
