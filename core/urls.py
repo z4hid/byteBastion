3e8bc966b0f1df24f00eab58066f11c037acba6f
z4hid <zahidhasan9297@gmail.com>
2024-10-30 01:20:58 +0600

    core app update

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,14 @@
 # urls.py
 from django.urls import path
-from . views import HomeView
+from . views import HomeView, SolutionsView, contact_view, success_view
+from . import views
+
+
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('solutions/', SolutionsView.as_view(), name='solutions'),
+    path('contact/', contact_view, name='contact'),
+    path('success/', success_view, name='success'),
+    path('passwords/', views.password_list, name='password_list'),
+    path('add_passowrd/', views.add_password, name='add_password'),
 ]
