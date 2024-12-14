7790b6dc0ea61291df7e060df3d8a22a76ec385e
z4hid <zahidhasan9297@gmail.com>
2024-12-14 23:16:52 +0600

    leak application admin updated

--- leak/admin.py
+++ leak/admin.py
@@ -4,8 +4,10 @@ from django.contrib import admin
 # leak/admin.py
 from django.contrib import admin
 from .models import LeakResult
+from .models import LeakResult
+
 @admin.register(LeakResult)
 class LeakCheckResultAdmin(admin.ModelAdmin):
-    list_display = ('username_or_email', 'found_count', 'checked_at')
+    list_display = ('username_or_email', 'found_count', 'created_at')
     search_fields = ('username_or_email',)
-    readonly_fields = ('checked_at',)
+    readonly_fields = ('created_at',)
